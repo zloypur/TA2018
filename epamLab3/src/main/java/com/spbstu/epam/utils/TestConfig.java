7b34c9942ce4c3a71266bd5552f7f4eadb5c03b7
zloypur <zloypur@gmail.com>
2018-04-10 12:14:56 +0300

    Lab3 done

--- epamLab3/src/main/java/com/spbstu/epam/utils/TestConfig.java
+++ epamLab3/src/main/java/com/spbstu/epam/utils/TestConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 import static org.aeonbits.owner.Config.Sources;
 
 @Sources({"classpath:test.properties"})
-public interface TestConfig extends Config{
+public interface TestConfig extends Config {
 
     @Key("webdriver.chrome.driver")
     String pathToDriver();
