59e2b198d2124cb8404ba86e779db4ca2716accf
zloypur <zloypur@gmail.com>
2018-04-25 11:47:06 +0300

    add lab5

---
+++ epamLab5/src/test/java/com/spbstu/epam/WebsiteTest.java
@@ -0,0 +1,124 @@
+package com.spbstu.epam;
+
+import com.codeborne.selenide.Configuration;
+import com.spbstu.epam.utils.TestConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.spbstu.epam.EpamTestWebsiteSelenide.*;
+import static com.spbstu.epam.enums.HOME_PAGE_DATA.*;
+import static com.spbstu.epam.enums.differentElementsPage.DIFFERENT_ELEMENTS_PAGE_CHECKBOX_TEXT.LEFT_CHECKBOX_TEXT;
+import static com.spbstu.epam.enums.differentElementsPage.DIFFERENT_ELEMENTS_PAGE_CHECKBOX_TEXT.MID_RIGHT_CHECKBOX_TEXT;
+import static com.spbstu.epam.enums.differentElementsPage.DIFFERENT_ELEMENTS_PAGE_DROPDOWN_TEXT.DROPDOWN_TEXT_4;
+import static com.spbstu.epam.enums.differentElementsPage.DIFFERENT_ELEMENTS_PAGE_RADIO_TEXT.RIGHT_RADIO_TEXT;
+
+public class WebsiteTest {
+
+    /*
+     * Create BeforeSuite method which get properties from test\resources\test.properties and configuring Selenide
+     */
+    @BeforeSuite
+    public void beforeSuite() {
+        TestConfig config = ConfigFactory.create(TestConfig.class);
+        System.setProperty("webdriver.chrome.driver", config.pathToDriver());
+
+        Configuration.browser = "CHROME";
+        Configuration.timeout = 4000;
+    }
+
+    /*
+     * Create BeforeTest method which opens Chrome window, maximaized it, navigates to the test website
+     */
+    @BeforeTest
+    public void beforeTest() {
+        init();
+    }
+
+    /*
+     * Create Test method which makes all the checks from the specification using pageObjects
+     */
+    @Test
+    public void websiteTest() {
+        /*
+         * open site by url
+         */
+        homePageJDI.open();
+
+        /*
+         * log in website
+         */
+        homePageJDI.login(LOGIN.getValue().toString(), PASSWORD.getValue().toString());
+
+        /*
+         * user name check
+         */
+        homePageJDI.checkUserName(USERNAME.getValue().toString());
+
+        /*
+         * presence of images on the home page check
+         */
+        homePageJDI.checkImages((Integer) IMAGES_COUNT.getValue());
+
+        /*
+         * presence and correctness of under images texts on the home page check
+         */
+        homePageJDI.checkTexts((List<String>) BENEFITS_TEXT.getValue());
+
+        /*
+         * main header and text below check
+         */
+        homePageJDI.checkMainTitle((String) MAIN_HEADER_TEXT.getValue());
+        homePageJDI.checkMainText((String) HEADER_TEXT.getValue());
+
+        /*
+         * header service menu visibility and options check
+         */
+        homePageJDI.checkHeaderServiceMenuOptions((List<String>) HEADER_DROPDOWN_SERVICE_MENU_OPTIONS_TEXT.getValue());
+
+        /*
+         * left side service menu visibility and options check
+         */
+        homePageJDI.checkLeftSideServiceMenuOption((List<String>) LEFT_SIDE_SERVICE_MENU_OPTIONS_TEXT.getValue());
+
+        /*
+         * open other page through header menu Service -> Different Elements
+         */
+        homePageJDI.openDifferentElementsPage();
+
+        /*
+         * checks presence of all needed elements on the Different Elements page
+         */
+        differentElementsPageJDI.checkPageElements();
+
+        /*
+         * select required elements on the Different Elements page
+         */
+        differentElementsPageJDI.setCheckboxSelected(LEFT_CHECKBOX_TEXT.getValue());
+        differentElementsPageJDI.setCheckboxSelected(MID_RIGHT_CHECKBOX_TEXT.getValue());
+        differentElementsPageJDI.setRadioSelected(RIGHT_RADIO_TEXT.getValue());
+        differentElementsPageJDI.setDropdownValue(DROPDOWN_TEXT_4.getValue());
+
+        /*
+         * checks that logs are coinciding with performed actions
+         */
+        differentElementsPageJDI.checkLogs((List<String>) Arrays.asList(DROPDOWN_TEXT_4.getValue(),
+                RIGHT_RADIO_TEXT.getValue(), MID_RIGHT_CHECKBOX_TEXT.getValue(), LEFT_CHECKBOX_TEXT.getValue()));
+
+        /*
+         * unselect required elements
+         */
+        differentElementsPageJDI.setCheckboxUnselected(LEFT_CHECKBOX_TEXT.getValue());
+        differentElementsPageJDI.setCheckboxUnselected(MID_RIGHT_CHECKBOX_TEXT.getValue());
+
+        /*
+         * check that logs are coinciding with performed actions
+         */
+        differentElementsPageJDI.checkLogs(Arrays.asList(LEFT_CHECKBOX_TEXT.getValue(),
+                MID_RIGHT_CHECKBOX_TEXT.getValue()));
+    }
+}
