6c3841e5e493fce55493f6bb2af4229c1455329b
Dmitry_Lebedev1 <Dmitry_Lebedev1@epam.com>
2018-05-21 11:56:16 +0300

    resolve conflicts

--- epamLab2/src/test/java/com/spbstu/epam/WebsiteTest.java
+++ epamLab2/src/test/java/com/spbstu/epam/WebsiteTest.java
@@ -1,10 +1,7 @@
 package com.spbstu.epam;
 
-<<<<<<< HEAD
-import com.sun.xml.internal.bind.v2.TODO;
-=======
 import com.spbstu.epam.utils.TestConfig;
->>>>>>> 614a8218a7ea2631bb2960c6ad2cbbbcc0379fe3
+
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,34 +13,11 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-<<<<<<< HEAD
-// TODO you have to format your code and remove useless imports
-public class WebsiteTest {
 
-    // TODO it will be better if you create ENUM(in a different file) for store this sort of data
-    public static final String TEST_WEBSITE_URL = ConfigFactory.create(TestConfig.class).homePageURL();
-    public static final String TEST_WEBSITE_TITLE = "Index Page";
-    public static final String LOGIN = "epam";
-    public static final String PASSWORD = "1234";
-    public static final String USERNAME = " PITER CHAILOVSKII";
-    public static final int IMAGES_COUNT = 4;
-    public static final int TEXTS_COUNT = 4;
-    public static final List<String> BENEFITS_TEXT = Arrays.asList("To include good practices\nand ideas from successful\nEPAM projec",
-            "To be flexible and\ncustomizable",
-            "To be multiplatform",
-            "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…");
-    public static final String MAIN_HEADER_TEXT = "EPAM FRAMEWORK WISHES…";
-    public static final String HEADER_TEXT = "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT," +
-            " SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, " +
-            "QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT " +
-            "DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.";
-    // !TODO
-=======
 import static com.spbstu.epam.enums.HOME_PAGE_DATA.*;
 
 public class WebsiteTest {
 
->>>>>>> 614a8218a7ea2631bb2960c6ad2cbbbcc0379fe3
 
     private WebDriver driver;
 
@@ -51,15 +25,10 @@ public class WebsiteTest {
      * Create BeforeSuite method which get properties from test\resources\test.properties
      */
     @BeforeSuite
-<<<<<<< HEAD
-    public void beforeSuite (){ // <--
-        // TODO this not really good idea couse in case if you have more than one property you will recreate the whole TestConfig...
-        System.setProperty("webdriver.chrome.driver", ConfigFactory.create(TestConfig.class).pathToDriver());
-=======
+
     public void beforeSuite (){
         TestConfig config = ConfigFactory.create(TestConfig.class);
         System.setProperty("webdriver.chrome.driver", config.pathToDriver());
->>>>>>> 614a8218a7ea2631bb2960c6ad2cbbbcc0379fe3
     }
 
     /*
@@ -99,12 +68,8 @@ public class WebsiteTest {
         /*
          * log in website
          */
-<<<<<<< HEAD
-        EpamTestWebsite.homePage.login(LOGIN, PASSWORD);
-        // TODO what is the reason why you are using shielding character ? From my point of view it will work pretty good as is...
-=======
+
         EpamTestWebsite.homePage.login((String)LOGIN.getValue(), (String)PASSWORD.getValue());
->>>>>>> 614a8218a7ea2631bb2960c6ad2cbbbcc0379fe3
         Assert.assertTrue(EpamTestWebsite.homePage.isLoggedIn(),
                 "Logout button isn\'t displayed. Login wasn\'t successful.");
 
